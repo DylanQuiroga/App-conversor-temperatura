afb812095c3bca91014e64ba291995734205be87
Dylan Quiroga Arancibia <dylan.quiroga@userena.cl>
2023-04-18 17:13:22 -0400

    act

--- Apk/app/src/main/java/com/example/praxis1/MainActivity.java
+++ Apk/app/src/main/java/com/example/praxis1/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.praxis1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    Button boton = findViewById(R.id.boton);
+    Spinner spinner = findViewById(R.id.spinner);
+    EditText valor = findViewById(R.id.valor);
+    EditText resultado = findViewById(R.id.resultado);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,3 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-
-    "dios mio"
